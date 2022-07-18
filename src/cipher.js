75bb15a1500c7bd020c9f43ea0b6f42e3a3cd5d1
laladiaz <mariaabelardadiaz@gmail.com>
2022-07-18 12:20:21 -0500

    Todos esto se hizo durante una OH con Carlos donde me explicó como simplificar mi pagina a una sola pantalla y como usar el boilerplate como Laboratoria lo concibio. Hay una prueba para ver que cipher.js y index.js se estan comunicando y que index.js se esta expresando (manejando el DOM) en index.html

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,8 @@
 const cipher = {
-  // ...
+  // ... objeto
+  ejemplo:function(){
+    return 'Soy un mensaje en la consola que anda por el bosque '
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const buttons = document.getElementById('buttons')
+const cipherButton = document.getElementById('cipherButton')
+
+/*
+cipherButton.addEventListener('click', function(){
+    buttons.innerHTML=`
+    <h1> JajajajajajaMuahaha</h1>
+    <textarea></textarea>
+    <button>cifra el texto</button>
+    `
+})
+*/
+
+/*
+cipherButton.addEventListener('click', function(){
+   const textarea=document.createElement('textarea')
+   const button=document.createElement('button')
+   button.textContent="Haz click para cifrar"
+
+   buttons.append(
+    textarea,button
+   )
+})
+*/
+
+cipherButton.addEventListener('click', function(){
+    console.log(cipher.ejemplo())
+})
