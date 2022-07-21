5cecf6f3ae7bc4848b97abc017f101ab0d48aca2
laladiaz <mariaabelardadiaz@gmail.com>
2022-07-21 18:37:40 -0500

    ultimo cambio antes de oh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import cipher from './cipher.js';
 // llamo a mis textareas y boton para cipher y las pongo como variables
 let sendCipher = document.getElementById("sendCipher");
-let offsetCipher = document.getElementById("offset-encrypt");
+let offsetCipher = document.getElementById("offset-encrypt").value;
 let encrypted = document.getElementById("encrypted");
 let msgToCipher = document.getElementById("msgInput").value
 //agrego un click listener para el boton click para cifrar
