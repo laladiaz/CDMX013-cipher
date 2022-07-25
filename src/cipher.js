3d4860b8bd20ebe0702c2c1eb973cd48929b84c7
laladiaz <mariaabelardadiaz@gmail.com>
2022-07-25 10:59:24 -0500

    ya con la formula de decode

--- src/cipher.js
+++ src/cipher.js
@@ -2,12 +2,21 @@ const cipher = {
   encode: function (letter, offset) {
     let newLetter = "";
     let letterCode = letter.charCodeAt(0);
-    if ((letterCode >=65) && (letterCode<=90)) {
-      var newLetterCode = (letterCode - 65) + offset %26 +65;
+    if ((letterCode >= 65) && (letterCode <= 90)) {
+      var newLetterCode = (letterCode - 65) + offset % 26 + 65;
     }
     newLetter = String.fromCharCode(newLetterCode);
     return newLetter;
-},
+  },
+  decode: function (letter, offset) {
+    let newLetter = "";
+    let letterCode = letter.charCodeAt(0);
+    if ((letterCode >= 65) && (letterCode <= 90)) {
+      var newLetterCode = (letterCode + 65) - offset % 26 + 65;
+    }
+    newLetter = String.fromCharCode(newLetterCode);
+    return newLetter;
+  },
 };
 
 export default cipher;
