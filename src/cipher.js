1132e64d0d0df0c9ffe3d8991e605b0d01dd46d9
laladiaz <mariaabelardadiaz@gmail.com>
2022-07-25 16:25:54 -0500

    ya imprime en el DOM, pero las formulas no quieren funcionar bien

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,28 @@
 const cipher = {
   encode: function (offset, string) {
-    //let offset = paseInt(offsetCipher.value);
     let encryptedMessage = "";
-    for (let i=0; i<string.legth; i++) {
-      let letterCode = string.charCodeAt(i)
-        if ((letterCode>=65) &&(letterCode<=90)) {
-          var newLetterCode = ((letterCode - 65 + offset) % 26 + 65);
-          var newLetter = String.fromCharCode(newLetterCode);
-        }
-        encryptedMessage += newLetter
+    for (let i = 0; i < string.length; i++) {
+      let letterCode = string.charCodeAt(i);
+      if ((letterCode >= 65) && (letterCode <= 90)) {
+        let newLetterCode = ((letterCode - 65) + offset % 26 + 65);
+        var newLetter = String.fromCharCode(newLetterCode);
       }
-      return encryptedMessage;
+      encryptedMessage += newLetter;
+    }
+    return encryptedMessage
   },
   decode: function (offset, string) {
-    let decodedLetter = "";
-    let letterCode = string.charCodeAt(0);
-    if ((letterCode >= 65) && (letterCode <= 90)) {
-      var newLetterCode = ((letterCode + 65 - offset) % 26 + 65);
+    let decodedMessage = "";
+    for (let i = 0; i < string.length; i++) {
+      let letter = string.charCodeAt(i);
+      if ((letter >= 65) && (letter <= 90)) {
+        let newLtCode = ((letter + 65) - offset % 26 + 65);
+        var newLt = String.fromCharCode(newLtCode);
+      }
+      decodedMessage += newLt;
     }
-    decodedLetter += String.fromCharCode(newLetterCode);
-    return decodedLetter;
-  },
+    return decodedMessage
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//import cipher from './cipher.js';
+import cipher from './cipher.js';
 //CIFRAR:
 // llamo a mis textareas y boton para cipher y las pongo como variables
 const cipherForm = document.getElementById("cipherForm");
@@ -6,35 +6,11 @@ const offsetCipher = document.getElementById("offset-encrypt");
 const encrypted = document.getElementById("encrypted");
 const msgInput = document.getElementById("msgInput");
 
-// este fragmento le pide al codigo que escuche el input en la ventana y 
-//let originalMessage = msgInput.value
-/*msgInput.addEventListener("input", function (e) {
-    originalMessage = e.target.value;
-});*/
-function encode (offset, string) {
-    //let offset = paseInt(offsetCipher.value);
-    let encryptedMessage = "";
-    for (let i=0; i<string.legth; i++) {
-      let letterCode = string.charCodeAt(i)
-        if ((letterCode>=65) &&(letterCode<=90)) {
-          var newLetterCode = ((letterCode - 65 + offset) % 26 + 65);
-          let newLetter = String.fromCharCode(newLetterCode);
-          encryptedMessage += newLetter
-          return encryptedMessage;
-        }
-      }
-      
-  }
-
-function startEncryption() {
-    let originalMessage = msgInput.value
-    let offsetEncode = parseInt(offsetCipher.value);
-    encrypted.value = encode(offsetEncode, originalMessage);
-}
-
 cipherForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    startEncryption();
+    e.preventDefault()
+});
+cipherForm.addEventListener("submit", function () {
+    encrypted.value = cipher.encode(offsetCipher.value, msgInput.value);
 });
 
 //DESCIFRAR
@@ -44,28 +20,9 @@ const offsetDecode = document.getElementById("offset-decode");
 const msgInputDecode = document.getElementById("msgCode");
 const msgDecoded = document.getElementById("decoded");
 
-// este fragmento le pide al codigo que escuche el input en la ventana y 
-let encodeMessage = msgInputDecode.value
-msgInputDecode.addEventListener("input", function (e) {
-    encodeMessage = e.target.value;
-    encodeMessage = encodeMessage.replace(/[^A-Z]/, "");
-
-    e.target.value = encodeMessage;
-});
-
-function startDecode() {
-    //creamos un string vacio para guardar el mensaje codificado
-    let decodedMessage = "";
-    let shift = parseInt(offsetDecode.value);
-    //el for.. of loop  va a guardar en la variable letter un array de las letras del mensaje original
-    for (let letterShifted of encodeMessage) {
-        decodedMessage += cipher.decode(shift, letterShifted);
-    }
-    //console.log(encryptedMessage);
-    msgDecoded.value = decodedMessage;
-}
-
 decodeForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    startDecode();
+    e.preventDefault()
+});
+decodeForm.addEventListener("submit", function () {
+    msgDecoded.value = cipher.decode(offsetDecode.value, msgInputDecode.value)
 });
