a2257f49dd2468073f2225ad9d8d283127936862
laladiaz <mariaabelardadiaz@gmail.com>
2022-07-25 09:38:20 -0500

    escribi la funcion decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,28 @@
 const cipher = {
-  encode(string, offset) {
+  encode: function (string, offset) {
     let msgEncrypted = "";
     for (let i = 0; i < string.length; i++) {
       let letter = string[i]
       let x = letter.charCodeAt(i)
       if ((x >= 65) && (x <= 90)) {
-        String.fromCharCode(((x - 65 + offset) % 26) + 65);
+        String.fromCharCode(((x - 65) + offset % 26) + 65);
       }
       msgEncrypted += letter;
     }
     return msgEncrypted;
   },
+decode: function (string, offset) {
+  let msgDecrypted = "";
+    for (let i = 0; i < string.length; i++) {
+      let letter = string[i]
+      let x = letter.charCodeAt(i)
+      if ((x >= 65) && (x <= 90)) {
+        String.fromCharCode(((x + 65) -offset % 26) + 65);
+      }
+      msgDecrypted += letter;
+    }
+    return msgDecrypted;
+}
 };
 
 export default cipher;
