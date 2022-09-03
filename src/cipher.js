a7ce42c2762ee6dc4b1fa18e8ee42040bbebc4a8
laladiaz <mariaabelardadiaz@gmail.com>
2022-09-03 10:14:49 -0500

    copies the cipher object from sinBotones branch in cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,40 @@
 const cipher = {
-  encode: function (string, offset) {
-    let msgEncrypted = "";
-    for (let i = 0; i < string.length; i++) {
-      let letter = string[i]
-      let x = letter.charCodeAt(i)
-      if ((x >= 65) && (x <= 90)) {
-        String.fromCharCode(((x - 65 + offset) % 26) + 65);
-      }
-      msgEncrypted += letter;
+  encode: function (offset, string) {
+    if (!offset >1 || typeof string !== "string")  {
+      throw new TypeError("Ingresa valores");
     }
-    return msgEncrypted;
+    let encryptedMessage = "";
+    for (let i = 0; i < string.length; i++) {
+      let letterCode = string.charCodeAt(i);
+      if ((letterCode >= 65) && (letterCode <= 90)) {
+        var newLetterCode = ((letterCode - 65 + offset) % 26 + 65);
+      } else if ((letterCode >= 97) && (letterCode <=122)) {
+        newLetterCode = ((letterCode -97 +offset) % 26 + 97);
+      } else {
+        newLetterCode = letterCode;
+      }
+      let newLetter = String.fromCharCode(newLetterCode);
+      encryptedMessage += newLetter;
+    }
+    return encryptedMessage
   },
-decode: function (string, offset) {
-  let msgDecrypted = "";
-    for (let i = 0; i < string.length; i++) {
-      let letter = string[i]
-      let x = letter.charCodeAt(i)
-      if ((x >= 65) && (x <= 90)) {
-        String.fromCharCode(((x + 65) -offset % 26) + 65);
-      }
-      msgDecrypted += letter;
+  decode: function (offset, string) {
+    if (typeof string !=="string" || !offset >1) {
+      throw new TypeError ("Ingresa valores");
     }
-    return msgDecrypted;
-}
+    let decodedMessage = "";
+    for (let i = 0; i < string.length; i++) {
+      let letter = string.charCodeAt(i);
+      if ((letter >= 65) && (letter <= 90)) {
+        var newLtCode = ((letter + 65 - offset) % 26 + 65);
+      } else if ((letter >= 97) && (letter <=122)) {
+        newLtCode = ((letter- 122 - offset) % 26 + 122);
+      }
+      let newLt = String.fromCharCode(newLtCode);
+      decodedMessage += newLt;
+    }
+    return decodedMessage
+  }
 };
 
 export default cipher;
